2a3a2a5164e637df9e429bfef1cd3e413893ee6d
unknown <madileesmith@gmail.com>
2021-08-29 15:30:56 -0500

    added notes to JS

--- assets/script/script.js
+++ assets/script/script.js
@@ -3,20 +3,20 @@ const numbers = "1234567890";
 const symbols = "!@#$%^&*_-+=";
 
 
-
+// defining variables
 const passwordTxt = document.getElementById("password");
 const length = document.getElementById("length");
 const incNumbers = document.getElementById("numbers");
 const incSymbols = document.getElementById("symbols");
 const generateBtn = document.getElementById("generate");
-
+// button click functionality
 generateBtn.addEventListener("click", () => {
   let characters = alpha;
   incNumbers.checked ? (characters += numbers) : "";
   incSymbols.checked ? (characters += symbols) : "";
   passwordTxt.value = generatePassword(length.value, characters);
 });
-
+// for loop for randomly generating the password 
 const generatePassword = (length, characters) => {
   let password = "";
   for (let i = 0; i < length; i++) {
