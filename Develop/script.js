c77b65872c795b898b4401c3951d2a4a455a06e4
unknown <madileesmith@gmail.com>
2021-08-29 14:39:42 -0500

    ...misspelled characters; debugged code

--- Develop/script.js
+++ Develop/script.js
@@ -1,25 +1,22 @@
-// Assignment code here
 const alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const numbers = "1234567890";
 const symbols = "!@#$%^&*_-+=";
+
 
 
 const passwordTxt = document.getElementById("password");
 const length = document.getElementById("length");
 const incNumbers = document.getElementById("numbers");
 const incSymbols = document.getElementById("symbols");
-const generatePassword = document.getElementById("generate");
+const generateBtn = document.getElementById("generate");
 
-
-// Add event listener to generate button
 generateBtn.addEventListener("click", () => {
   let characters = alpha;
-  // ternary operators
   incNumbers.checked ? (characters += numbers) : "";
   incSymbols.checked ? (characters += symbols) : "";
   passwordTxt.value = generatePassword(length.value, characters);
 });
-// generate password function
+
 const generatePassword = (length, characters) => {
   let password = "";
   for (let i = 0; i < length; i++) {
